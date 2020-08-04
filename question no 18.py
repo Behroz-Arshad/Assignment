2e54e6521efad883bf6e0e22ca5c9dcfe38a3f80
Behroz-Arshad <69111034+Behroz-Arshad@users.noreply.github.com>
2020-08-04 18:22:41 +0500

    Update question no 18.py
    last was wrong upload

--- question no 18.py
+++ question no 18.py
@@ -1,11 +1,18 @@
+
 #Question no 18
+'''
+A pangram is a sentence that contains all the letters
+of the English alphabet at least once,
+for example: "The quick brown fox jumps over the lazy dog".
+Your task here is to write a function to check a sentence to see
+if it is a pangram or not.
+'''
 
-s="abcdefghijklmopqrstuvwxyz"
-z="behroz"
-
-for i in z:
-    if i in s:
-        pass
-print("yes")
-else:
-    print("no")
+#Code
+def ispangram(string): 
+    letter = "abcdefghijklmnopqrstuvwxyz"
+    for i in letter: 
+        if i not in string.lower(): 
+            return False
+  
+    return True
