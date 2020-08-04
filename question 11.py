cff7a60706c3f29ba3a983bb04de4750fd54c1e5
Behroz-Arshad <69111034+Behroz-Arshad@users.noreply.github.com>
2020-08-04 18:10:09 +0500

    Created
    Define a function generate_n_chars() that takes an integer n and a character c
    and returns a string, n characters long, consisting only of c:s.
    For example, generate_n_chars( 5,"x" )
    should return the string "xxxxx".
    (Python is unusual in that you can actually write an expression 5 * "x"
    that will evaluate to "xxxxx".
    For the sake of the
    exercise you should ignore that the problem can be solved in this manner ).

---
+++ question 11.py
@@ -0,0 +1,19 @@
+#Question no 11
+'''
+Define a function generate_n_chars() that takes an integer n and a character c
+and returns a string, n characters long, consisting only of c:s.
+For example, generate_n_chars( 5,"x" )
+should return the string "xxxxx".
+(Python is unusual in that you can actually write an expression 5 * "x"
+that will evaluate to "xxxxx".
+For the sake of the
+exercise you should ignore that the problem can be solved in this manner ).
+'''
+
+#Code
+
+def chars(n,c):
+    count=0
+    while count!=n:
+        print(c,end="")
+        count=count+1
